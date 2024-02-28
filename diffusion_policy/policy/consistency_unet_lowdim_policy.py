db75812d6d483f425e60ffcd1326d62f063e9a20
JoannaHXU <hang017@e.ntu.edu.sg>
2024-02-28 14:41:34 +0800

    rename consistency-lowdim files

--- diffusion_policy/policy/consistency_unet_lowdim_policy.py
+++ diffusion_policy/policy/consistency_unet_lowdim_policy.py
@@ -329,7 +329,4 @@ class ConsistencyUnetLowdimPolicy(BaseLowdimPolicy):
                 self.target_model_param_groups_and_shapes,
                 self.target_model_master_params,
             )
-        # print(self.target_model_master_params)
-
-
-
+        # print(self.target_model_master_params        
